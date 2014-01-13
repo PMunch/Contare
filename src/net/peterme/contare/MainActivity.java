a7f572d1baa7f408f7b7fa2c63ebd4064b59ef26
PMunch <peterme@peterme.net>
2014-01-13 22:39:51 +0100

    Wrote base app. Added: Buttons (Image and functionality) Background mechanism Layout

--- src/net/peterme/contare/MainActivity.java
+++ src/net/peterme/contare/MainActivity.java
@@ -2,21 +2,53 @@ package net.peterme.contare;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
-
+	
+	private TextView number;
+	private SharedPreferences settings;
+	private Editor editor;
+	private int num;
+	private String PREFS = "ContarePrefs";
+	private String TAG = "Contare";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		number = (TextView) findViewById(R.id.number);
+//		((ImageView) findViewById(R.id.plus))
+		
+		settings = getSharedPreferences(PREFS,0);
+		editor = settings.edit();
+		num = settings.getInt("number",0);
+		
+		number.setText(Integer.toString(num));
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	protected void onPause(){
+		super.onPause();
+		editor.putInt("number", num);
+		editor.commit();
 	}
-
+	protected void onResume(){
+		super.onResume();
+		settings = getSharedPreferences(PREFS,0);
+		editor = settings.edit();
+		num = settings.getInt("number",0);
+		number.setText(Integer.toString(num));
+	}
+	public void onClicked(View view){
+		if (((ImageView) view).getId()==R.id.minus){
+			num--;
+		}else{
+			num++;
+		}
+		number.setText(Integer.toString(num));
+	}
 }
